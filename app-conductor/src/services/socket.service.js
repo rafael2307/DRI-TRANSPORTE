fd9f6323dc80034705a4e33dc41c687088410a2d
rafael2307 <rafaelsegundom@gmail.com>
2026-08-21 05:33:17 -0500

    Refactor SocketService methods for consistency

--- app-conductor/src/services/socket.service.js
+++ app-conductor/src/services/socket.service.js
@@ -5,83 +5,104 @@ const SOCKET_URL = 'http://localhost:3000'; // Update with your local IP for phy
 class SocketService {
     socket = null;
 
-    connect(token) {
-        this.socket = io(SOCKET_URL, {
-            auth: { token }
-        });
+connect(token) {
+    this.socket = io(SOCKET_URL, {
+        auth: { token }
+    });
 
-        this.socket.on('connect', () => {
-            console.log('Connected to socket server');
-        });
+    this.socket.on('connect', () => {
+        console.log('Connected to socket server');
+    });
 
-        this.socket.on('disconnect', () => {
-            console.log('Disconnected from socket server');
-        });
+    this.socket.on('disconnect', () => {
+        console.log('Disconnected from socket server');
+    });
 
-        this.socket.on('unauthorized', (err) => {
-            console.error('Socket no autorizado:', err?.message);
-        });
+    this.socket.on('unauthorized', (err) => {
+        console.error('Socket no autorizado:', err?.message);
+    });
+}
+
+onNewTripRequest(callback) {
+    if (this.socket) {
+        this.socket.on('newTripRequest', callback);
     }
+}
 
-    onNewTripRequest(callback) {
-        if (this.socket) {
-            this.socket.on('newTripRequest', callback);
-        }
+acceptTrip(data) {
+    if (this.socket) {
+        this.socket.emit('acceptTrip', data);
     }
+}
 
-    acceptTrip(data) {
-        if (this.socket) {
-            this.socket.emit('acceptTrip', data);
-        }
+driverArrived(tripId) {
+    if (this.socket) {
+        this.socket.emit('driverArrived', { tripId });
     }
+}
 
-    driverArrived(tripId) {
-        if (this.socket) {
-            this.socket.emit('driverArrived', { tripId });
-        }
+startTrip(tripId) {
+    if (this.socket) {
+        this.socket.emit('startTrip', { tripId });
     }
+}
 
-    startTrip(tripId) {
-        if (this.socket) {
-            this.socket.emit('startTrip', { tripId });
-        }
+completeTrip(tripId) {
+    if (this.socket) {
+        this.socket.emit('completeTrip', { tripId });
     }
+}
 
-    completeTrip(tripId) {
-        if (this.socket) {
-            this.socket.emit('completeTrip', { tripId });
-        }
+cancelTrip(tripId, reason) {
+    if (this.socket) {
+        this.socket.emit('cancelTrip', { tripId, reason });
     }
+}
 
-    cancelTrip(tripId, reason) {
-        if (this.socket) {
-            this.socket.emit('cancelTrip', { tripId, reason });
-        }
+sendMessage(tripId, message) {
+    if (this.socket) {
+        this.socket.emit('sendMessage', { tripId, message });
     }
+}
 
-    sendMessage(tripId, message) {
-        if (this.socket) {
-            this.socket.emit('sendMessage', { tripId, message });
-        }
+onNewMessage(callback) {
+    if (this.socket) {
+        this.socket.on('newMessage', callback);
     }
+}
 
-    onNewMessage(callback) {
-        if (this.socket) {
-            this.socket.on('newMessage', callback);
-        }
+updateLocation(lat, lng, serviceType) {
+    if (this.socket) {
+        this.socket.emit('updateLocation', { lat, lng, serviceType });
     }
+}
 
-    updateLocation(lat, lng, serviceType) {
-        if (this.socket) {
-            this.socket.emit('updateLocation', { lat, lng, serviceType });
-        }
+// Chequeo de bienestar/fatiga: solo llega si el conductor dio su
+// consentimiento explícito (ver pantalla de ajustes). El backend nunca
+// lo usa para penalizar, solo para preguntar cómo va el conductor.
+onWellnessCheckIn(callback) {
+    if (this.socket) {
+        this.socket.on('wellnessCheckIn', callback);
     }
+}
 
-    disconnect() {
-        if (this.socket) {
-            this.socket.disconnect();
-        }
+respondWellnessCheckIn(checkInId, response) {
+    if (this.socket) {
+        this.socket.emit('wellnessCheckInResponse', { checkInId, response });
     }
+}
+
+onWellnessCheckInAck(callback) {
+    if (this.socket) {
+        this.socket.on('wellnessCheckInAck', callback);
+    }
+}
+
+disconnect() {
+    if (this.socket) {
+        this.socket.disconnect();
+    }
+}
 }
 
 export const socketService = new SocketService();
