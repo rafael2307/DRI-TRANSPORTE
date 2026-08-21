a5cfb2b112d01dd62d568f12fb35b604c41c3c11
rafael2307 <rafaelsegundom@gmail.com>
2026-08-21 05:39:34 -0500

    Add AiModule to TripsModule imports

--- backend/src/trips/trips.module.ts
+++ backend/src/trips/trips.module.ts
@@ -5,15 +5,17 @@ import { TripsService } from './trips.service';
 import { PaymentsModule } from '../payments/payments.module';
 import { LocationModule } from '../location/location.module';
 import { TripsController } from './trips.controller';
+import { AiModule } from '../ai/ai.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Trip]),
     PaymentsModule,
     forwardRef(() => LocationModule),
-  ],
+    AiModule,
+    ],
   controllers: [TripsController],
   providers: [TripsService],
   exports: [TripsService],
 })
-export class TripsModule {}
+  export class TripsModule {}
